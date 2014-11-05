7789d478cb4aebeea3b9f87dd50c60d72f18ed1f
Rasmus Kildevæld <admin@softshag.dk>
2014-11-05 07:35:15 +0100

    Initial Commit

---
+++ FAObjectMap/FAObjectMap.h
@@ -0,0 +1,13 @@
+//
+//  FAObjectMap.h
+//  FAObjectMap
+//
+//  Created by Rasmus Kildevaeld on 05/11/14.
+//  Copyright (c) 2014 Rasmus Kildevaeld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FAObjectMap : NSObject
+
+@end
---
+++ FAObjectMap/FAObjectMap.m
@@ -0,0 +1,13 @@
+//
+//  FAObjectMap.m
+//  FAObjectMap
+//
+//  Created by Rasmus Kildevaeld on 05/11/14.
+//  Copyright (c) 2014 Rasmus Kildevaeld. All rights reserved.
+//
+
+#import "FAObjectMap.h"
+
+@implementation FAObjectMap
+
+@end
