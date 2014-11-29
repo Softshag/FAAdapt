f3dd84072e68f598336f4df52ca49cb8fb7e2bd5
Rasmus Kildevæld <admin@softshag.dk>
2014-11-30 00:51:38 +0100

    added descriptionspec

---
+++ Example/Tests/DescriptionSpec.m
@@ -0,0 +1,31 @@
+//
+//  FAAdaptTests.m
+//  FAAdaptTests
+//
+//  Created by Rasmus Kildevæld on 11/29/2014.
+//  Copyright (c) 2014 Rasmus Kildevæld. All rights reserved.
+#import <FAPropertyDescription.h>
+#import <FAArrayDescription.h>
+
+#import "Fixtures.h"
+
+SPEC_BEGIN(Description)
+
+describe(@"FADescription", ^{
+    
+    context(@"constructor", ^{
+        it(@"can construct", ^{
+            
+        });
+        
+    });
+    
+    context(@"map", ^{
+        it(@"can map values", ^{
+                        
+        });
+    });
+    
+});
+
+SPEC_END
